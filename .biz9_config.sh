e0470bfbe52880d37b1baf163170f41aa93eaaae
server-217 <coderz@bossappz.com>
2023-11-14 13:10:08 +0000

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,12 +1,12 @@
 BIZ9_SERVER_VERSION='5.2.2'
-APP_VERSION='1.0.3'
+APP_VERSION='1.0.1'
 APP_ID='217';
 APP_TITLE='Server-217';
 APP_TITLE_ID='server-217';
 EC2_IP_ADDR=${BIZ9_BOX_IP_217}
 EC2_DEPLOY_DIR="/home/admin/www/217/server"
 EC2_KEY_FILE=${BIZ9_BOX_SSH_217}
-REPO_URL='github.com';
+REPO_URL='git@github.com:bossappz/server-217.git';
 MONGO_PORT='27019'
 PROJECT_LIST=(website-100 website-101 mobile-243 mobile-242 mobile-241 mobile-240 mobile-253 mobile-252 mobile-250 mobile-249 mobile-248 mobile-247 mobile-246 mobile-245 mobile-244 mobile-251 mobile-234
 mobile-235 mobile-236 mobile-237 mobile-238 mobile-239)
