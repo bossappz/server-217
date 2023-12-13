5606f9dd9aee5d73f7e42afe97346d9b33973a3d
server-217 <coderz@bossappz.com>
2023-12-13 01:13:07 +0000

    updated db website-204

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,5 +1,5 @@
 BIZ9_SERVER_VERSION='5.2.2'
-APP_VERSION='1.0.1'
+APP_VERSION='1.0.2'
 APP_ID='217';
 APP_TITLE='Server-217';
 APP_TITLE_ID='server-217';
@@ -8,5 +8,5 @@ SERVER_DEPLOY_DIR="/home/admin/www/217/server"
 SSH_KEY=${BIZ9_BOX_SSH_217}
 REPO_URL='git@github.com:bossappz/server-217.git';
 MONGO_PORT='27019'
-PROJECT_LIST=(website-100 website-101 mobile-243 mobile-242 mobile-241 mobile-240 mobile-253 website-252 mobile-250 mobile-249 mobile-248 mobile-247 mobile-246 mobile-245 mobile-244 mobile-251 mobile-234
+PROJECT_LIST=(website-100 website-101 website-204 mobile-243 mobile-242 mobile-241 mobile-240 mobile-253 website-252 mobile-250 mobile-249 mobile-248 mobile-247 mobile-246 mobile-245 mobile-244 mobile-251 mobile-234
 mobile-235 mobile-236 mobile-237 mobile-238 mobile-239)
